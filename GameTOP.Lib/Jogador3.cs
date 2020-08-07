5648c1bac79c697de01a7a1b91a5fd1ff03bd711
EdersonAntonio <zerocol780@gmail.com>
2020-08-06 22:26:11 -0300

    First Commit

---
+++ GameTOP.Lib/Jogador3.cs
@@ -0,0 +1,22 @@
+using GameTOP.Interface;
+
+namespace GameTOP.Lib
+{
+    public class Jogador3 : iJogador
+    {
+        public string Chuta()
+        {
+            return "Teste chuta. \n";
+        }
+
+        public string Corre()
+        {
+            return "Teste corre. \n";
+        }
+
+        public string Passe()
+        {
+            return "Teste passe. \n";
+        }
+    }
+}
